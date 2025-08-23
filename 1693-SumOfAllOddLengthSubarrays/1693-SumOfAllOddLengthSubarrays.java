0fe3927f8f936f81efda5278f978ca0f3a4d4e53
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:26:47 +0530

    Create 1693-SumOfAllOddLengthSubarrays

---
+++ 1693-SumOfAllOddLengthSubarrays/1693-SumOfAllOddLengthSubarrays.java
@@ -0,0 +1,10 @@
+// Last updated: 8/23/2025, 10:26:47 PM
+class Solution {
+        public int sumOddLengthSubarrays(int[] A) {
+        int res = 0, n = A.length;
+        for (int i = 0; i < n; ++i) {
+            res += ((i + 1) * (n - i) + 1) / 2 * A[i];
+        }
+        return res;
+    }
+}
