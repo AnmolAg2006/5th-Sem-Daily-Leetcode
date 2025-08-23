8c6c1504751245c737a87e0c40a1aa4f8a644272
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:26:22 +0530

    Create 1960-CheckIfTheSentenceIsPangram

---
+++ 1960-CheckIfTheSentenceIsPangram/1960-CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,18 @@
+// Last updated: 8/23/2025, 10:26:22 PM
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int c[]=new int[26];
+        for(int i=0;i<sentence.length();i++)
+        {
+            c[sentence.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(c[i]==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
