f9be93646059697165506f9d328920dd0d7a1213
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:24:59 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,14 @@
+// Last updated: 8/23/2025, 10:24:59 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return (...args) => "Hello World";
+    
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
