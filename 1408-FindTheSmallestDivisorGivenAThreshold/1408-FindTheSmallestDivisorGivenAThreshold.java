38887b3c7585150a42abea64e1d09eb8d13d6b18
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:27:20 +0530

    Create 1408-FindTheSmallestDivisorGivenAThreshold

---
+++ 1408-FindTheSmallestDivisorGivenAThreshold/1408-FindTheSmallestDivisorGivenAThreshold.java
@@ -0,0 +1,24 @@
+// Last updated: 8/23/2025, 10:27:20 PM
+class Solution {
+    public boolean isPossible(int piles[], int mid, int h) {
+        int hours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            hours += (piles[i] + mid - 1) / mid;
+        }
+        return hours <= h;
+    }
+    public int smallestDivisor(int[] piles, int h) {
+         int s = 1;
+        int e = Arrays.stream(piles).max().getAsInt();
+
+        while (s < e) {
+            int mid = s + (e - s) / 2;
+            if (isPossible(piles, mid, h)) {
+                e = mid;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return s;
+    }
+}
