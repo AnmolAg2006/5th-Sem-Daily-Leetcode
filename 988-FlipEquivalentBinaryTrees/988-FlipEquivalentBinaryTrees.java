139c767406a58fffddb6ad741ffb00ba48a978f9
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:27:41 +0530

    Create 988-FlipEquivalentBinaryTrees

---
+++ 988-FlipEquivalentBinaryTrees/988-FlipEquivalentBinaryTrees.java
@@ -0,0 +1,49 @@
+// Last updated: 8/23/2025, 10:27:41 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1==null && root2==null)
+        {
+            return true;
+        }
+        if(root1==null || root2==null)
+        {
+            return false;
+        }
+         if(root1.val!=root2.val)
+        {
+            return false;
+        }
+        return  (rec(root1.left,root2.right) && rec(root1.right,root2.left))||(rec(root1.left,root2.left) && rec(root1.right,root2.right));
+    }
+    public static boolean rec(TreeNode root1,TreeNode root2)
+    {
+        if(root1==null && root2==null)
+        {
+            return true;
+        }
+        if(root1==null || root2==null)
+        {
+            return false;
+        }
+        if(root1.val!=root2.val)
+        {
+            return false;
+        }
+        return (rec(root1.left,root2.right) && rec(root1.right,root2.left))||(rec(root1.left,root2.left) && rec(root1.right,root2.right));
+    }
+}
