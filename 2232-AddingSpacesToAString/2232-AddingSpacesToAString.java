c9075b90295f27aeb1bed0f3a34fc56fb588e8f1
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:25:59 +0530

    Create 2232-AddingSpacesToAString

---
+++ 2232-AddingSpacesToAString/2232-AddingSpacesToAString.java
@@ -0,0 +1,18 @@
+// Last updated: 8/23/2025, 10:25:59 PM
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        int j=0;
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(j<spaces.length && i==spaces[j]){
+                ans.append(" ");
+                j++;
+                ans.append(s.charAt(i));
+            }
+            else{
+                ans.append(s.charAt(i));
+            }
+        }
+        return ans.toString();
+    }
+}
