dc07a4f4f865bc349dac27685f68c07fa2d2cc75
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:25:24 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,7 @@
+// Last updated: 8/23/2025, 10:25:23 PM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        
+        return new double[]{celsius+273.15,celsius*1.8+32};
+    }
+}
