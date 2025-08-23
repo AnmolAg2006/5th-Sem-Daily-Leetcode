a977a19e221d1df29da664a11ec265ca98d8aad9
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:25:58 +0530

    Create 2235-CapitalizeTheTitle

---
+++ 2235-CapitalizeTheTitle/2235-CapitalizeTheTitle.java
@@ -0,0 +1,14 @@
+// Last updated: 8/23/2025, 10:25:58 PM
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] s = title.toLowerCase().split(" ");
+
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].length() > 2) {
+                s[i] = Character.toUpperCase(s[i].charAt(0)) + s[i].substring(1);
+            }
+        }
+
+        return String.join(" ", s);
+    }
+}
