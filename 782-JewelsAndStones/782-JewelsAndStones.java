192557efb54494620632a7c982c354fb479c2fbc
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:28:05 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.java
@@ -0,0 +1,17 @@
+// Last updated: 8/23/2025, 10:28:05 PM
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int c=0;
+        for(int i=0;i<jewels.length();i++)
+        {
+            for(int j=0;j<stones.length();j++)
+            {
+                if(jewels.charAt(i)==stones.charAt(j))
+                {
+                    c++;
+                }
+            }
+        }
+        return c;
+    }
+}
