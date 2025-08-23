af46b342cca3c9a25114dbb0375464d0ce8a0cdd
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:24:21 +0530

    Create 3461-FindTheMinimumAreaToCoverAllOnesI

---
+++ 3461-FindTheMinimumAreaToCoverAllOnesI/3461-FindTheMinimumAreaToCoverAllOnesI.java
@@ -0,0 +1,23 @@
+// Last updated: 8/23/2025, 10:24:21 PM
+class Solution {
+    public int minimumArea(int[][] grid) {
+        int maxi=-1;
+        int maxj=-1;
+        int mini=grid.length;
+        int minj=grid[0].length;
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    mini=Math.min(mini,i);
+                    minj=Math.min(minj,j);
+                    maxi=Math.max(maxi,i);
+                    maxj=Math.max(maxj,j);
+                }
+            }
+        }
+        return (maxi-mini+1)*(maxj-minj+1);
+    }
+}
