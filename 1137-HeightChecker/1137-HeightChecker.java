9048f1a825acb2da98ba7c2400b81d1adcccbd07
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:27:36 +0530

    Create 1137-HeightChecker

---
+++ 1137-HeightChecker/1137-HeightChecker.java
@@ -0,0 +1,14 @@
+// Last updated: 8/23/2025, 10:27:35 PM
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] sorted = Arrays.copyOf(heights, heights.length);
+        Arrays.sort(sorted);
+        int ans = 0;
+        for(int i = 0; i < heights.length; i++) {
+            if(heights[i] != sorted[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
