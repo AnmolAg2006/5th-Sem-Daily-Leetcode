20c733366d7196054344c015183c41ba94efa8f4
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:28:25 +0530

    Create 572-SubtreeOfAnotherTree

---
+++ 572-SubtreeOfAnotherTree/572-SubtreeOfAnotherTree.java
@@ -0,0 +1,50 @@
+// Last updated: 8/23/2025, 10:28:25 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public static boolean isIdentical(TreeNode root,TreeNode subRoot)
+    {
+        if(root==null && subRoot==null)
+        {
+            return true;
+        }
+        
+        if(root==null || subRoot==null)
+        {
+            return false;
+        }
+        if(root.val!=subRoot.val)
+        {
+            return false;
+        }
+        return isIdentical(root.left,subRoot.left)&& isIdentical(root.right,subRoot.right);
+    }
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null)
+        {
+            return false;
+        }
+        if(root.val==subRoot.val)
+        {
+            if(isIdentical(root,subRoot))
+            {
+                return true;
+            }
+
+        }
+        return isSubtree(root.left,subRoot)|| isSubtree(root.right,subRoot);
+    }
+}
