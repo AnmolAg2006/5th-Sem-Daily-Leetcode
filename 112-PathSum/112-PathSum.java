f0bc4a20ad83ecbdb8c1fa8df635336978056024
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:30:01 +0530

    Create 112-PathSum

---
+++ 112-PathSum/112-PathSum.java
@@ -0,0 +1,29 @@
+// Last updated: 8/23/2025, 10:30:01 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root==null)
+        {
+            return false;
+        }
+        if(root.left==null && root.right==null)
+        {
+            return targetSum-root.val==0;
+        }
+        return hasPathSum(root.left,targetSum-root.val)||hasPathSum(root.right,targetSum-root.val);
+    }
+}
