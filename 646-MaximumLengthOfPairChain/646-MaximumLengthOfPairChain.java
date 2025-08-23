a83ac7cde5bbc7bab759a8a4fa3106b48944410c
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:28:16 +0530

    Create 646-MaximumLengthOfPairChain

---
+++ 646-MaximumLengthOfPairChain/646-MaximumLengthOfPairChain.java
@@ -0,0 +1,17 @@
+// Last updated: 8/23/2025, 10:28:15 PM
+class Solution {
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs,(a,b)->Integer.compare(a[1],b[1]));
+        int et=pairs[0][1];
+        int ans=1;
+        for(int i=1;i<pairs.length;i++)
+        {
+            if(pairs[i][0]>et)
+            {
+                ans++;
+                et=pairs[i][1];
+            }
+        }
+        return ans;
+    }
+}
