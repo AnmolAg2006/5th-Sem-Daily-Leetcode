5f94fc2b53e5483e1c7d3b05f6a729fb8093f222
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-08-23 22:28:33 +0530

    Create 535-EncodeAndDecodeTinyurl

---
+++ 535-EncodeAndDecodeTinyurl/535-EncodeAndDecodeTinyurl.java
@@ -0,0 +1,17 @@
+// Last updated: 8/23/2025, 10:28:33 PM
+public class Codec {
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        return longUrl;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return shortUrl;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
