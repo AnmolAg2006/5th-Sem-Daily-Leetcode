d8cc846e697b8bd0a65744810f91b7042a285d1b
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-09-22 10:52:11 +0530

    File updated at 9/22/2025, 10:52:11 AM

--- 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.java
+++ 3005-CountElementsWithMaximumFrequency/3005-CountElementsWithMaximumFrequency.java
@@ -1,30 +1,56 @@
-// Last updated: 9/22/2025, 10:44:47 AM
+// Last updated: 9/22/2025, 10:52:11 AM
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        HashMap<Integer,Integer>hm=new HashMap<>();
-        for(int i:nums)
-        {
-            hm.put(i,hm.getOrDefault(i,0)+1);
-        }
-        int max=-1;
-        int sum=0;
-        System.out.println(hm);
-        for(int i:hm.keySet())
-        {
-            if(hm.get(i)>max)
-            {
-                sum=hm.get(i);
-                max=hm.get(i); 
-                continue;
+        int n = nums.length;
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++) {
+            if(nums[i]>max) {
+                max = nums[i];
             }
-            if(hm.get(i)==max)
-            {
-                System.out.print(i+" ");
-                sum+=max;
-            }
-            
         }
-        
-        return sum;
+        int[] arr = new int[max+1];
+        for(int i=0;i<n;i++) {
+            arr[nums[i]]++;
+        }
+        int maxFreq = 0;
+        int count = 0;
+        for(int i=0;i<arr.length;i++) {
+            if(maxFreq<arr[i]) {
+                maxFreq = arr[i];
+                count = 1;
+            } else if (maxFreq == arr[i]) {
+                Math.max(maxFreq,++count);
+                //count++;
+            }
+
+        }
+        return count*maxFreq;
+        /*Map<Integer, Integer> countMap = new HashMap<>();
+        int initialCount = 1;
+        int maxCount = 0;
+        int ret = 0;
+        for(int i=0;i<nums.length;i++) {
+             if (countMap.containsKey(nums[i])) {
+                int count = countMap.get(nums[i]);
+                count++;
+                countMap.put(nums[i], count);
+                if (maxCount<count) {
+                    maxCount = count;
+                }
+             } else {
+                countMap.put(nums[i], initialCount);
+             }
+             
+        }
+        if (maxCount == 0) {
+            maxCount = 1;
+        }
+        for(Integer i : countMap.keySet()) {
+            if (maxCount == countMap.get(i)) {
+                ret += maxCount;
+            }
+        }
+        return ret;
+    }*/
     }
 }
