590a93995cbb9b27a4e84e8370573fb5c534ed06
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-09-27 17:57:53 +0530

    File updated at 9/27/2025, 5:57:52 PM

--- 88-MergeSortedArray/88-MergeSortedArray.java
+++ 88-MergeSortedArray/88-MergeSortedArray.java
@@ -1,10 +1,26 @@
-// Last updated: 8/23/2025, 10:30:12 PM
+// Last updated: 9/27/2025, 5:57:52 PM
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i=m;i<m+n;i++)
+        int i=m-1;
+        int j=n-1;
+        int ans=n+m-1;
+        while(i>=0 && j >= 0)
         {
-            nums1[i]=nums2[i-m];
+            if(nums1[i]<nums2[j])
+            {
+                nums1[ans]=nums2[j];
+                j--;
+            }
+            else {
+                nums1[ans]=nums1[i];
+                i--;
+
+            }
+            ans--;
         }
-        Arrays.sort(nums1);
+        while(j>=0)
+        {
+            nums1[ans--]=nums2[j--];
+        }
     }
 }
