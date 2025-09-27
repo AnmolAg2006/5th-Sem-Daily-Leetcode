01a6bf7fcdcf485eb61fee76b5fa2ff8914e159f
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-09-27 17:03:06 +0530

    File updated at 9/27/2025, 5:03:05 PM

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.java
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -1,10 +1,20 @@
-// Last updated: 9/27/2025, 5:00:16 PM
+// Last updated: 9/27/2025, 5:03:05 PM
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(Math.pow(num,0.5)!=(int)Math.pow(num,0.5))
-        {
-            return false;
+        int start = 1;
+        int end = num;
+        while(start <= end){
+            long mid = (end-start)/2+start;
+            if(mid*mid == num){
+                return true;
+            }
+            else if(mid*mid < num){
+                start = (int)(mid+1);
+            }
+            else {
+                end = (int)(mid-1);
+            }
         }
-        return true;
+        return false;
     }
 }
