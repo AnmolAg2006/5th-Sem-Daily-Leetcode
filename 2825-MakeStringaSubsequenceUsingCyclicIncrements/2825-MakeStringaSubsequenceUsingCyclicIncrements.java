8aaae6ba70f2967757e97be1c92a71f01ecbf16e
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-09-28 11:32:05 +0530

    Create 2825-MakeStringaSubsequenceUsingCyclicIncrements

---
+++ 2825-MakeStringaSubsequenceUsingCyclicIncrements/2825-MakeStringaSubsequenceUsingCyclicIncrements.java
@@ -0,0 +1,23 @@
+// Last updated: 9/28/2025, 11:32:03 AM
+class Solution {
+    public boolean canMakeSubsequence(String str1, String str2) {
+        int i=0;
+        int j=0;
+        while(i<str1.length() && j<str2.length())
+        {
+            if(str1.charAt(i)==str2.charAt(j) || (str1.charAt(i)!='z' && str1.charAt(i)+1==str2.charAt(j)) || (str1.charAt(i)=='z' && str2.charAt(j)=='a') )
+            {
+                i++;
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        if(j==str2.length())
+        {
+            return true;
+        }
+        return false;
+    }
+}
