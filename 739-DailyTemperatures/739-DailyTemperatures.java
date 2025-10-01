1688789e115dda30174d76a77f4b6386a0ab30f0
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-10-01 10:18:33 +0530

    File updated at 10/1/2025, 10:18:32 AM

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,4 +1,4 @@
-// Last updated: 10/1/2025, 10:18:17 AM
+// Last updated: 10/1/2025, 10:18:32 AM
 class Solution {
     public int[] dailyTemperatures(int[] temp) {
         int n=temp.length;
@@ -6,19 +6,13 @@ class Solution {
         Stack<Integer>st=new Stack<>();
         for(int i=0;i<n;i++)
         {
-            if(!st.isEmpty() && temp[st.peek()]<temp[i])
-            {
                 while(!st.isEmpty() && temp[st.peek()]<temp[i])
                 {
                     ans[st.peek()]=i-st.pop();
                 }
-            }
+            
                 st.push(i);
             
-        }
-        while(!st.isEmpty())
-        {
-            ans[st.pop()]=0;
         }
         return ans;
     }
