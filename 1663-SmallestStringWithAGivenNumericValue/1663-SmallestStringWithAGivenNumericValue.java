e5d32b8d838ceda07f6ef3c4051af8a6c758a4b5
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-11-09 00:15:29 +0530

    File updated at 11/9/2025, 12:15:27 AM

--- 1663-SmallestStringWithAGivenNumericValue/1663-SmallestStringWithAGivenNumericValue.java
+++ 1663-SmallestStringWithAGivenNumericValue/1663-SmallestStringWithAGivenNumericValue.java
@@ -1,25 +1,18 @@
-// Last updated: 11/9/2025, 12:15:15 AM
+// Last updated: 11/9/2025, 12:15:27 AM
 class Solution {
     public String getSmallestString(int n, int k) {
-        if(n==5 && k==130)
-        {
-            return "zzzzz";
+        char[] res = new char[n];
+        Arrays.fill(res, 'a');
+        k -= n; // because all are 'a' initially (value 1 each)
+
+        int i = n - 1;
+        while (k > 0) {
+            int add = Math.min(25, k); // max we can add to make 'z'
+            res[i] = (char) (res[i] + add);
+            k -= add;
+            i--;
         }
-        int f[]=new int[n];
-        k-=n;
-        int z=k/25;
-        int r=k%25;
-        StringBuilder ans=new StringBuilder();
-        z=n-z-1;
-        for(int i=0;i<z;i++)
-        {
-            ans.append('a');
-        }
-        ans.append((char)('a'+r));
-        for(int i=z+1;i<n;i++)
-        {
-            ans.append('z');
-        }
-        return ans.toString();
+
+        return new String(res);
     }
 }
