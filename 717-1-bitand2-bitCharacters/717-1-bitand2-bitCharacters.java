028ba6824b035573ba879706a780dae934980993
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-11-18 13:55:12 +0530

    Create 717-1-bitand2-bitCharacters

---
+++ 717-1-bitand2-bitCharacters/717-1-bitand2-bitCharacters.java
@@ -0,0 +1,17 @@
+// Last updated: 11/18/2025, 1:55:11 PM
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int n=bits.length;
+        for(int i=0;i<n;i++)
+        {
+            if(bits[i]==1)
+            {
+                i++;
+            }
+            else if(bits[i]==0 && i==n-1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
