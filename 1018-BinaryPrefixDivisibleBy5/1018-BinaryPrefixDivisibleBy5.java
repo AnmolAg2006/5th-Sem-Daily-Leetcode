ea8af18a241fce0b6b2b8db5fd6efa2546e26eb8
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-11-24 14:37:40 +0530

    File updated at 11/24/2025, 2:37:39 PM

--- 1018-BinaryPrefixDivisibleBy5/1018-BinaryPrefixDivisibleBy5.java
+++ 1018-BinaryPrefixDivisibleBy5/1018-BinaryPrefixDivisibleBy5.java
@@ -1,15 +1,40 @@
-// Last updated: 11/24/2025, 2:35:22 PM
-class Solution {
-    public List<Boolean> prefixesDivBy5(int[] nums) {
-        List<Boolean> ans=new ArrayList<>();
-        int sum=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            sum=(sum*2+nums[i])%5;
-            if(sum%5==0) ans.add(true);
-            else ans.add(false);
-        }
-        return ans;
+// Last updated: 11/24/2025, 2:37:39 PM
+import java.util.*;
 
+class Solution {
+
+  public static final int MIN_LENGTH = 1;
+  public static final int MAX_LENGTH = 100_000;
+
+  // 1 - 1
+  // 11 - 3
+  // 111 - 7
+  // 1111 - 15
+  // 11111 - 31
+  // 111111 - 63
+  // 1111111 - 127
+  public List<Boolean> prefixesDivBy5(int[] nums) {
+    List<Boolean> list = null;
+    if(nums != null) {
+      list = new LinkedList<>();
+      final int length = nums.length;
+      int binary = 0;
+      for(int i = 0; i < length; ++i) {
+        int bit = nums[i];
+        binary <<= 1;
+        binary += bit;
+
+        binary %= 5;
+        list.add(binary == 0 ? Boolean.TRUE : Boolean.FALSE);
+      }
     }
+    return list;
+  }
+
+  static {
+    Solution s = new Solution();
+    for(int i = 0; i < 1000; ++i) {
+      s.prefixesDivBy5(new int[]{1, 0, 1});
+    }
+  }
 }
