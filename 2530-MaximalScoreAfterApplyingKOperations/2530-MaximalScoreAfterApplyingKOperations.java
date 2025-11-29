c6f7ba7464482b3c562e932bcbaef109c1c88d24
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-11-29 15:49:39 +0530

    File updated at 11/29/2025, 3:49:38 PM

--- 2530-MaximalScoreAfterApplyingKOperations/2530-MaximalScoreAfterApplyingKOperations.java
+++ 2530-MaximalScoreAfterApplyingKOperations/2530-MaximalScoreAfterApplyingKOperations.java
@@ -1,4 +1,4 @@
-// Last updated: 11/29/2025, 3:48:47 PM
+// Last updated: 11/29/2025, 3:49:38 PM
 1class Solution {
 2    public long maxKelements(int[] nums, int k) {
 3        PriorityQueue<Integer>pq=new PriorityQueue<>(Collections.reverseOrder());
@@ -8,7 +8,7 @@
 7        {
 8            int temp=pq.poll();
 9            ans+=temp;
-10            pq.add((int)Math.ceil(temp/3.0));
+10            pq.add((temp + 2) / 3);
 11        }
 12        return ans;
 13    }
