b497268c713827f45cf0312053ed787ac4390d94
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-12-06 13:36:07 +0530

    File updated at 12/6/2025, 1:36:06 PM

--- 948-BagofTokens/948-BagofTokens.java
+++ 948-BagofTokens/948-BagofTokens.java
@@ -1,27 +1,27 @@
-// Last updated: 12/6/2025, 1:33:33 PM
-1class Solution {
-2    public int bagOfTokensScore(int[] tokens, int power) {
-3        Arrays.sort(tokens);
-4        int lo = 0;
-5        int hi = tokens.length - 1;
-6        int s=0;
-7        int ms=0;
-8        while (lo <= hi) {
-9            if (tokens[lo] <= power) {
-10                power-=tokens[lo];
-11                s++;
-12                ms=Math.max(ms,s);
-13                lo++;
-14            } else if(s>=1) {
-15                    power+=tokens[hi];
-16                    hi--;
-17                    s--;
-18                } 
-19                
-20            else{
-21                break;
-22            }
-23        }
-24        return ms;
-25    }
-26}
+// Last updated: 12/6/2025, 1:36:06 PM
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int curr=0;
+        int max=0;
+        int n=tokens.length-1;
+        int i=0;int j=n;
+        while(i<=j){
+            if(power>=tokens[i]){
+                        power-=tokens[i++];
+                      curr++;
+                    }
+            
+            else if(curr>0){
+                power+=tokens[j];
+                curr-=1;
+                j--;
+            }
+            else 
+            break;
+            max=Math.max(curr,max);
+            }
+        
+        return max;
+    }
+}
