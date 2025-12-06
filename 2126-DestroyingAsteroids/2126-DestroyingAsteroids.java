1d8b4d8d283c3a65c79efff4170ff2c3bf404cb2
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-12-06 15:07:11 +0530

    File updated at 12/6/2025, 3:07:10 PM

--- 2126-DestroyingAsteroids/2126-DestroyingAsteroids.java
+++ 2126-DestroyingAsteroids/2126-DestroyingAsteroids.java
@@ -1,17 +1,15 @@
-// Last updated: 12/6/2025, 3:05:40 PM
-1class Solution {
-2    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
-3        Arrays.sort(asteroids);
-4        long masss=mass;
-5        for(int i=0;i<asteroids.length;i++)
-6        {
-7            if(asteroids[i]>masss)
-8            {
-9                return false;
-10            }
-11            masss+=asteroids[i];
-12
-13        }
-14        return true;
-15    }
-16}
+// Last updated: 12/6/2025, 3:07:10 PM
+class Solution {
+    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+        Arrays.sort(asteroids);
+        long currentMass=mass;
+        for(int asteroid:asteroids){
+        if(currentMass < asteroid){
+            return false;
+        }
+        currentMass+=asteroid;
+        
+    }
+    return true;
+}
+}
