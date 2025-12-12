3cccdf1c566b3189abf9586fe54dd06103e82268
Anmol Agarwal <agarwalnaina1982@gmail.com>
2025-12-12 22:12:09 +0530

    File updated at 12/12/2025, 10:12:08 PM

--- 2829-DeterminetheMinimumSumofak-avoidingArray/2829-DeterminetheMinimumSumofak-avoidingArray.java
+++ 2829-DeterminetheMinimumSumofak-avoidingArray/2829-DeterminetheMinimumSumofak-avoidingArray.java
@@ -1,19 +1,19 @@
-// Last updated: 12/12/2025, 10:09:51 PM
+// Last updated: 12/12/2025, 10:12:08 PM
 1class Solution {
 2    public int minimumSum(int n, int k) {
 3        HashSet<Integer>hs=new HashSet<>();
 4        int ans=0;
 5        int i=1;
-6        while(hs.size()<n)
+6        while(n>0)
 7        {
 8            if(!hs.contains(k-i))
 9            {
 10                ans+=i;
 11                hs.add(i);
-12            }
-13            i++;
-14        }
-15        System.out.print(hs);
+12                n--;
+13            }
+14            i++;
+15        }
 16        return ans;
 17    }
 18}
